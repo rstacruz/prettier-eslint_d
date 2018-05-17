0a675117e6fed3bcdffd8d49448a90949a03b41a
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-05-17 19:30:43 +0800

    Delegate to prettier-eslint-cli
    Anything that's not --stdin will be handled by prettier-eslint-cli.

---
+++ README.md
@@ -0,0 +1,37 @@
+# prettier-eslint_d
+
+> [Prettier](https://prettier.io) and [Eslint](https://eslint.org), but faster
+
+## Installation
+
+```bash
+yarn add --dev prettier-eslint_d
+```
+
+```bash
+./prettier-eslint_d --help
+```
+
+## Spacemacs installation
+
+See: <https://gist.github.com/rstacruz/a2361d000a88e49472c4419116edaccf>
+
+## Also see
+
+- [eslint_d](https://yarn.pm/eslint_d)
+- [prettier_d](https://yarn.pm/prettier_d)
+
+## Thanks
+
+**prettier-eslint_d** © 2018+, Rico Sta. Cruz. Released under the [MIT] License.<br>
+Authored and maintained by Rico Sta. Cruz with help from contributors ([list][contributors]).
+
+> [ricostacruz.com](http://ricostacruz.com) &nbsp;&middot;&nbsp;
+> GitHub [@rstacruz](https://github.com/rstacruz) &nbsp;&middot;&nbsp;
+> Twitter [@rstacruz](https://twitter.com/rstacruz)
+
+[![](https://img.shields.io/github/followers/rstacruz.svg?style=social&label=@rstacruz)](https://github.com/rstacruz) &nbsp;
+[![](https://img.shields.io/twitter/follow/rstacruz.svg?style=social&label=@rstacruz)](https://twitter.com/rstacruz)
+
+[MIT]: http://mit-license.org/
+[contributors]: http://github.com/rstacruz/prettier-eslint_d/contributors
--- bin/prettier-eslint_d.js
+++ bin/prettier-eslint_d.js
@@ -2,10 +2,14 @@ const rminimist = require('rminimist')
 const dargs = require('dargs')
 const spawn = require('child_process').spawn
 const debug = require('debug')('prettier-eslint_d')
+const join = require('path').resolve
+
+/**
+ * These are discarded
+ */
 
 const UNSUPPORTED = {
   boolean: ['no-bracket-spacing', 'jsx-bracket-same-line', 'use-tabs'],
-
   string: ['arrow-parens', 'single-quote', 'config-precedence']
 }
 
@@ -31,7 +35,6 @@ const OPTIONS = {
   ],
   string: [
     'cursor-offset',
-    'help',
     'loglevel',
     'parser',
     'print-width',
@@ -57,60 +60,87 @@ const OPTIONS = {
  */
 
 function run() /*: Promise<void> */ {
+  const argv /*: Array<string> */ = process.argv.slice(2)
+  const flags /*: Flags */ = rminimist(argv, OPTIONS)
+
+  if (flags.version) {
+    console.log(require('../package.json').version)
+    process.exit(0)
+  } else if (flags.stdin) {
+    return spawnPiped({
+      prettierArgs: dargs(flags),
+      eslintArgs: ['--stdin', '--fix-to-stdout']
+    })
+  } else {
+    require('.bin/prettier-eslint')
+    return Promise.resolve()
+  }
+}
+
+/**
+ * Spawns prettier and eslint in a piped fashion (`prettier_d | eslint_d`).
+ *
+ * @example
+ * This is equivalent to doing `prettier_d --stdin | eslint_d --stdin
+ * --fix-to-stdout`.
+ *
+ *     spawnPiped({
+ *       prettierArgs: ['--stdin'],
+ *       eslintArgs: ['--stdin', '--fix-to-stdout']
+ *     })
+ */
+
+function spawnPiped ({ prettierArgs, eslintArgs }) /*: Promise<void> */ {
   return new Promise((resolve, reject) => {
-    const argv /*: Array<string> */ = process.argv.slice(2)
-    const flags /*: Flags */ = rminimist(argv, OPTIONS)
     const nodePath /*: string */ = process.argv[0]
 
     const prettierCmd /*: Array<string> */ = [
       getPrettierPath(),
-      ...dargs(flags)
+      ...prettierArgs
     ]
     const eslintCmd /*: Array<string> */ = [
       getEslintPath(),
-      '--stdin',
-      '--fix-to-stdout',
-      ...(flags['_'] || [])
+      ...eslintArgs
     ]
 
-    debug('run(): spawn prettier', prettierCmd)
-    const prettier = spawn(nodePath, prettierCmd, {
-      stdio: ['inherit', 'pipe', 'inherit']
+    const prettier /*: Process */ = spawn(nodePath, prettierCmd, {
+      stdio: [process.stdin, 'pipe', process.stderr]
     })
 
-    debug('run(): spawn eslint', eslintCmd)
-    const eslint = spawn(nodePath, eslintCmd, {
-      stdio: ['pipe', 'inherit', 'inherit']
-    })
-    prettier.on('stdout', (data) => {
-      eslint.write(data)
+    const eslint /*: Process */ = spawn(nodePath, eslintCmd, {
+      stdio: [prettier.stdout, 'pipe', process.stderr]
     })
 
-    prettier.on('close', (code, signal) => {
+    eslint.stdout.on('data', (data /*: Buffer */) => {
+      process.stdout.write(data)
+    })
+
+    prettier.on('close', (code /*: ?number */, signal /*: string */) => {
       debug('run(): prettier died', { code, signal })
-      process.exit(code)
+      process.exit(code || 1)
       resolve()
     })
 
-    eslint.on('close', (code, signal) => {
+    eslint.on('close', (code /*: ?number */, signal /*: string */) => {
       debug('run(): eslint died', { code, signal })
-      process.exit(code)
-      resolve()
+      if (code !== 0) {
+        process.exit(code)
+        resolve()
+      }
     })
-
-    // console.log(flags)
-    // console.log(dargs(flags))
-    // console.log({ prettierPath })
-    // console.log({ eslintPath })
   })
 }
 
 function getEslintPath() /*: string */ {
-  return require.resolve('eslint_d/bin/eslint_d.js')
+  return require.resolve('.bin/eslint_d')
 }
 
 function getPrettierPath() /*: string */ {
-  return require.resolve('prettier_d/bin/prettier_d.js')
+  return require.resolve('.bin/prettier_d')
+}
+
+function getPrettierEslintPath() /*: string */ {
+  return require.resolve('.bin/prettier_d')
 }
 
 /*
@@ -118,11 +148,8 @@ function getPrettierPath() /*: string */ {
  */
 
 if (!module.parent) {
-  run()
-    .catch(err => {
-      console.error(err.stack)
-      process.exit(err.code)
-    })
-
-
+  run().catch(err => {
+    console.error(err.stack)
+    process.exit(err.code)
+  })
 }
